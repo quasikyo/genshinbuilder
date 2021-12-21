d5fbb895f01450572668a15f54c18f4da4d8475b
Michael Rehman <michael@michaelrehman.com>
2021-12-21 18:21:37 -0500

    rename navbar component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -3,14 +3,14 @@ import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { NMenu } from 'naive-ui';
 import { supabase } from '../supabase';
-import { generateMenuItems } from './_navbar';
+import { generateMenuItems } from './navbar';
 
 const router = useRouter();
 const allRoutes = router.options.routes;
 
 let menuItems = ref(generateMenuItems(allRoutes, supabase.auth.user()));
 onMounted(
-	() => supabase.auth.onAuthStateChange((event, session) => {
+	() => supabase.auth.onAuthStateChange((_, session) => {
 		menuItems.value = generateMenuItems(allRoutes, session?.user);
 	})
 );
